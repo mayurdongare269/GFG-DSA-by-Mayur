80f4509dd15f1b5ae18b9f6284228b92c167e8ce
Mayur Dongare <144617577+mayurdongare269@users.noreply.github.com>
2026-03-03 10:36:11 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Find nth root of m/find-nth-root-of-m.java
+++ Difficulty: Medium/Find nth root of m/find-nth-root-of-m.java
@@ -1,29 +1,23 @@
 class Solution {
     public int nthRoot(int n, int m) {
         // code here
-        int low = 1, high = m;
+        int low = 0, high = m;
         
         while(low <= high) {
             int mid = low + (high - low) / 2;
             
-            long pow = power(mid, n);
+            int ans = 1;
+            for(int i = 0; i < n; i++) {
+                ans *= mid;
+                if(ans > m) break;
+            }
             
-            if(pow == m) return mid;
-            else if(pow < m) low = mid + 1;
+            if(ans == m) return mid;
+            
+            if(ans < m) low = mid + 1;
             else high = mid - 1;
         }
         
         return -1;
-    }
-    
-    private long power(int base, int exp) {
-        long result = 1;
-        
-        for(int i = 0; i < exp; i++) {
-            result *= base;
-            if(result > Integer.MAX_VALUE) break;
-        }
-        
-        return result;
     }
 }
